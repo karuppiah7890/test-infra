7ca8bd6f9dc8a93166d041f0c3b8424cf5de46c4
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2022-01-10 16:06:50 +0530

    install zip and run docker management + workload cluster e2e tests
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

--- docker-images/tce/entrypoint.sh
+++ docker-images/tce/entrypoint.sh
@@ -14,3 +14,10 @@ git clone https://github.com/vmware-tanzu/community-edition
 pushd community-edition
 
 ./test/build-tce.sh
+
+./test/install-dependencies.sh
+
+# run docker management + workload cluster test like this -
+# ./test/docker/run-tce-docker-managed-cluster.sh
+# or use below make target! :)
+make docker-management-and-cluster-e2e-test
